39da1b5294b59d077d7761277f87e52d608ecc5b
di.chen <eraserses@163.com>
2021-09-07 21:02:52 +0800

    change conf

--- conf.py
+++ conf.py
@@ -3,7 +3,7 @@ from sqlalchemy.sql.expression import false
 
 DATABASES = {
     "world": {
-        "uri": "mysql+pymysql://root:passwd@localhost:3307/acore_world?charset=utf8mb4",
+        "uri": "mysql+pymysql://root:root@localhost:3306/world?charset=utf8mb4",
         "echo": False,
     }
 }
--- main.py
+++ main.py
@@ -5,6 +5,4 @@ import sqlalchemy as sa
 
 with world_engine.connect() as conn:
     creator = VendorCreator(conn)
-    creator.do_create(
-        "warrior super vendor", ["Willey's Portable Howitzer", "Primed Musket"]
-    )
+    print(creator.vendor_pk)
