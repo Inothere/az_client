124efcfe7ad236f9e8b6f9f0afe093f2458ae6ae
di.chen <eraserses@163.com>
2021-09-08 22:45:33 +0800

    兼容中文

--- spell/read.py
+++ spell/read.py
@@ -1,5 +1,5 @@
 import typing
-from dbcpy.dbc_file import DBCFile
+from dbc import CustomDBCFile
 from dbcpy.records.spell_record import SpellRecord
 import csv
 
@@ -13,31 +13,36 @@ class BaseReader:
     def to_csv(self, output: str):
         with open(self.filename, "rb") as f:
             data = []
-            dbc = DBCFile.from_file(f, self.record_type)
+            dbc = CustomDBCFile.from_file(f, self.record_type)
             for record in dbc.records:
                 data.append(self.form_csv_row(record))
         if not data:
             return
         headers = list(data[0].keys())
         with open(output, "w") as f:
-            writer = csv.DictWriter(f, headers, dialect=csv.excel_tab)
+            writer = csv.DictWriter(f, headers, dialect=csv.unix_dialect)
             writer.writeheader()
             writer.writerows(data)
 
-    def form_csv_row(record: typing.Any) -> typing.Dict:
+    def form_csv_row(self, record: typing.Any) -> typing.Dict:
         raise NotImplementedError()
 
 
 class SpellReader(BaseReader):
     record_type = SpellRecord
 
-    def form_csv_row(record: typing.Any) -> typing.Dict:
+    def form_csv_row(self, record: typing.Any) -> typing.Dict:
         return {
             "id": record.entry,
-            "name": record.name.en_us,
-            "subname": record.subname.en_us,
+            "name": record.name.de_de,
+            "subname": record.subname.de_de,
             "cooldown": record.cooldown,
             "global_cooldown": record.global_cooldown,
             "spell_level": record.spell_level,
             "mana_cost": record.mana_cost,
         }
+
+
+if __name__ == '__main__':
+    reader = SpellReader(r"D:\prog\ayasecore\core\data\dbc\spell.dbc")
+    reader.to_csv("spell.csv")
